79122f2fa39b5e3a2f47f918dd5b5cfcebea165e
brok02 <kelly.brownsberger@csgi.com>
2015-02-10 08:49:09 -0500

    playing

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,6 @@
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+$Env:Path += ";" + $Env.ALLUSERSPROFILE + "\chocolatey\bin"
 
 choco install puppet
 
