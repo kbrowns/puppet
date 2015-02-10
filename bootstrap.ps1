46b93e709a0d30026d81fe99badc94f702a139b3
brok02 <kelly.brownsberger@csgi.com>
2015-02-10 12:43:44 -0500

    playing

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,19 +1,19 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$Env:Path += ";" + $Env.ALLUSERSPROFILE + "\chocolatey\bin"
+$env:path += ";" + $Env.ALLUSERSPROFILE + "\chocolatey\bin"
 
 choco install puppet
 
 choco install git
 
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+$env:path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 
-If (Test-Path 'puppet'){
-	Remove-Item 'puppet' -Force -Recurse
+If (test-path 'puppet'){
+	remove-item 'puppet' -Force -Recurse
 }
 
 git clone https://github.com/kbrowns/puppet.git
 
-Set-Location -Path puppet
+set-location -path puppet
 
-bootstrap-puppet.ps1
+puppet/bootstrap-puppet.ps1
